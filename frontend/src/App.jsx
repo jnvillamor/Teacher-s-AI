3b7385213d682ec7c83b842d5865b08dc4c8fcb0
darkwanderland <josekayed@gmail.com>
2023-06-07 23:23:33 +0800

    Finished Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,9 @@ function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="md:grid md:grid-cols-5 h-screen bg-dark-gray">
       <Sidebar />
+      <div className="col-span-4 text-4xl p-6">Home Page</div>
     </div>
   );
 }
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,75 @@
 import React from "react";
+import { useState } from "react";
+import Logo from "../assets/Logo.png";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import AppsRoundedIcon from "@mui/icons-material/AppsRounded";
+import ExitToAppRoundedIcon from "@mui/icons-material/ExitToAppRounded";
+import Button from "@mui/material/Button";
+
+const buttonStyle = {
+  backgroundColor: "black",
+  justifyContent: "flex-start",
+  textTransform: "none",
+  fontFamily: "Poppins",
+  fontSize: "1.25rem",
+  borderRadius: "0.5rem",
+};
 
 const Sidebar = () => {
-  return <div className="bg-black h-full w-12.5rem">ghv</div>;
+  const [active, setActive] = useState("Courses");
+
+  const handleClick = (page) => {
+    setActive(page);
+  };
+
+  return (
+    <div className="bg-black col-span-1 p-6 rounded-r-lg flex flex-col flex">
+      <div className="flex justify-center">
+        <img src={Logo} alt="Logo" className="h-12 m-8 top-8 sm:top-16m" />
+      </div>
+      <div className="flex flex-col mt-[32px] sm:top-54m px-8">
+        <Button
+          variant="contained"
+          startIcon={<HomeRoundedIcon />}
+          style={Object.assign({}, buttonStyle, {
+            backgroundColor: active === "Home" ? "gray" : "black",
+          })}
+          className="w-full"
+          onClick={(e) => handleClick("Home")}
+        >
+          Home
+        </Button>
+
+        <Button
+          variant="contained"
+          startIcon={<AppsRoundedIcon />}
+          style={Object.assign({}, buttonStyle, {
+            backgroundColor: active === "Courses" ? "gray" : "black",
+          })}
+          className="w-full"
+          onClick={(e) => handleClick("Courses")}
+        >
+          Courses
+        </Button>
+
+        <Button
+          variant="contained"
+          startIcon={<ExitToAppRoundedIcon />}
+          style={{
+            backgroundColor: "black",
+            justifyContent: "flex-start",
+            textTransform: "none",
+            fontFamily: "Poppins",
+            fontSize: "1.25rem",
+            borderRadius: "0.5rem",
+          }}
+          className="w-full"
+        >
+          Log Out
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
