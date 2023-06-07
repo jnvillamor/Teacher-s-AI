de607a3e436f7a9033d410e79aabfcf6205f9e9b
darkwanderland <josekayed@gmail.com>
2023-06-07 17:19:01 +0800

    Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react';
+import { useState } from "react";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
-    <div className='flex h-screen'>
-      <div className='m-auto'>
-        <div className='text-6xl text-red-600'>{count}</div>
-        <button className='px-6 py-2 rounded bg-green-800 hover:bg-green-600 text-white' type='button' onClick={() => setCount(count => count+1)}>
-          count+
-        </button>
-      </div>
+    <div>
+      <Sidebar />
     </div>
   );
 }
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sidebar = () => {
+  return <div className="bg-black h-full w-12.5rem">ghv</div>;
+};
+
+export default Sidebar;
